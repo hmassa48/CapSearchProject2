6c3d6419974dcb2db26a044945a9440c83a2799a
Haley Massa <59619226+hmassa48@users.noreply.github.com>
2020-07-22 10:53:15 -0500

    Add files via upload

---
+++ keras_unet/models/hyper_unet.py
@@ -0,0 +1,198 @@
+""" Keras Tuner LGG Segmentation """ 
+
+import kerastuner as kt
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras import layers
+from tensorflow.keras import backend
+
+from kerastuner.engine import hypermodel
+from ..metrics import iou, iou_thresholded
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import (
+    BatchNormalization,
+    Conv2D,
+    Conv2DTranspose,
+    MaxPooling2D,
+    Dropout,
+    SpatialDropout2D,
+    UpSampling2D,
+    Input,
+    concatenate,
+    multiply,
+    add,
+    Activation,
+)
+
+
+
+class HyperBasicUNet(hypermodel.HyperModel):
+    """A UNet HyperModel.
+    # Arguments:
+        include_top: whether to include the fully-connected
+            layer at the top of the network.
+        input_shape: Optional shape tuple, e.g. `(256, 256, 3)`.
+              One of `input_shape` or `input_tensor` must be
+              specified.
+        input_tensor: Optional Keras tensor (i.e. output of
+            `layers.Input()`) to use as image input for the model.
+              One of `input_shape` or `input_tensor` must be
+              specified.
+        classes: optional number of classes to classify images
+            into, only to be specified if `include_top` is True,
+            and if no `weights` argument is specified.
+        **kwargs: Additional keyword arguments that apply to all
+            HyperModels. See `kerastuner.HyperModel`.
+    """
+
+    def __init__(self,
+                 #include_top=True,
+                 input_shape=None,
+                 input_tensor=None,
+                 classes=None,
+                 **kwargs):
+
+        super(HyperBasicUNet, self).__init__(**kwargs)
+        
+        
+        #if include_top and classes is None:
+            #raise ValueError('You must specify `classes` when '
+                             #'`include_top=True`')
+
+        if input_shape is None and input_tensor is None:
+            raise ValueError('You must specify either `input_shape` '
+                             'or `input_tensor`.')
+
+        #self.include_top = include_top
+        self.input_shape = input_shape
+        self.input_tensor = input_tensor
+        self.classes = classes
+        
+        
+    def build(self, hp):
+        num_layers = hp.Int('layers', 3,6,1, default = 4)
+        filters = hp.Choice('filters', values = [16,32,64,128], default = 64)
+        kernel = hp.Choice('kernel', values = [3,5,7], default = 3)
+        #activation = hp.Choice('activation', values = ['relu', 'elu'])
+        use_batch_norm = hp.Boolean('batch_norm', default = False)
+        dropout = hp.Float('dropout', 0, 0.4, 0.05, default = 0)
+        #dropout_change_per_layer = hp.Float('drop_out_change_per_layer', 0.0,0.3,0.025)
+        use_dropout_on_upsampling = hp.Boolean('dropout_on_upsampling', default = False)
+        decoder_type = hp.Choice('decoder_type', values = ['simple', 'transpose'])
+        
+        #hyperparameters not tuning
+        dropout_type="standard"
+        output_activation="sigmoid"
+        #decoder_type = 'simple'
+        activation = 'relu'
+        strides = (2,2)
+        dropout_change_per_layer = 0.0
+        
+        # Build U-Net model
+        inputs = Input(self.input_shape)
+        x = inputs
+
+        down_layers = []
+        for l in range(num_layers):
+            x = conv2d_block(
+            inputs=x,
+            filters=filters,
+            use_batch_norm=use_batch_norm,
+            dropout=dropout,
+            dropout_type=dropout_type,
+            activation=activation,
+            )
+            down_layers.append(x)
+            x = MaxPooling2D((2, 2))(x)
+            dropout += dropout_change_per_layer
+            filters = filters * 2  # double the number of filters with each layer
+
+        x = conv2d_block(
+            inputs=x,
+            filters=filters,
+            use_batch_norm=use_batch_norm,
+            dropout=dropout,
+            dropout_type=dropout_type,
+            activation=activation,
+        )
+
+        if not use_dropout_on_upsampling:
+            dropout = 0.0
+            dropout_change_per_layer = 0.0
+
+        for conv in reversed(down_layers):
+            filters //= 2  # decreasing number of filters with each layer
+            dropout -= dropout_change_per_layer
+            if decoder_type == 'transpose':
+                x = Conv2DTranspose(filters, (3,3), strides=(2,2),
+                            padding='same', use_bias=not(use_batch_norm))(x)
+                if use_batch_norm:
+                    x = BatchNormalization()(x)
+                x = Activation(activation)(x)
+                x = concatenate([x, conv])
+                x = Conv2D(filters,(3,3),activation=activation,kernel_initializer='he_normal',padding='same',use_bias=not use_batch_norm)
+                if use_batch_norm:
+                    x = BatchNormalization()(x)
+                x = Activation(activation)(x)
+
+            else:
+                x = UpSampling2D(strides)(x)
+                x = concatenate([x, conv])
+                x = conv2d_block(
+                inputs=x,
+                filters=filters,
+                use_batch_norm=use_batch_norm,
+                dropout=dropout,
+                dropout_type=dropout_type,
+                activation=activation,
+            )
+
+        outputs = Conv2D(self.classes, (1, 1), activation=output_activation)(x)
+
+        
+        #model = Model(inputs=[inputs], outputs=[outputs])
+        
+        model = keras.Model(inputs, outputs, name='UNet')
+        optimizer_name = hp.Choice('optimizer', ['adam', 'rmsprop', 'sgd'], default='adam')
+        optimizer = keras.optimizers.get(optimizer_name)
+        optimizer.learning_rate = hp.Choice(
+                'learning_rate', [0.1, 0.01, 0.001], default=0.01)
+        model.compile(
+                optimizer=optimizer,
+                loss='binary_crossentropy',
+                metrics=[iou, iou_thresholded])
+        return model
+    
+def conv2d_block(
+        inputs,
+        use_batch_norm=True,
+        dropout=0.3,
+        dropout_type="standard",
+        filters=16,
+        kernel_size=(3, 3),
+        activation="relu",
+        kernel_initializer="he_normal",
+        padding="same"):
+
+        if dropout_type == "spatial":
+            DO = SpatialDropout2D
+        elif dropout_type == "standard":
+            DO = Dropout
+        else:
+            raise ValueError(
+                f"dropout_type must be one of ['spatial', 'standard'], got {dropout_type}"
+            )
+
+        c = Conv2D(filters,kernel_size,activation=activation,
+        kernel_initializer=kernel_initializer,
+        padding=padding,use_bias=not use_batch_norm)(inputs)
+        if use_batch_norm:
+            c = BatchNormalization()(c)
+        if dropout > 0.0:
+            c = Dropout(dropout)(c)
+        c = Conv2D(filters,kernel_size,activation=activation,
+            kernel_initializer=kernel_initializer,padding=padding,use_bias=not use_batch_norm)(c)
+        if use_batch_norm:
+            c = BatchNormalization()(c)
+        return c
