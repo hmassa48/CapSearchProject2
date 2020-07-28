aadbc9fd536921aa1bae2c56a8368660dfa3df6b
Haley Massa <59619226+hmassa48@users.noreply.github.com>
2020-07-28 18:07:34 -0500

    Update losses.py

--- keras_unet/losses.py
+++ keras_unet/losses.py
@@ -21,5 +21,7 @@ def dice_loss(y_true, y_pred,smooth = 1):
     return 1. - dc
 
 #This is the function used in work 
+#It is a combination of binary cross entropy and the dice loss function from above 
+#This was created to better evaluate the segmentation problem of returning a blank mask image with high accuracy
 def bce_dice_loss(y_true, y_pred):
     return binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
