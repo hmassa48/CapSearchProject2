a092977cf4e4c62c6b72de2959a7d739c37d920b
Haley Massa <59619226+hmassa48@users.noreply.github.com>
2020-07-30 22:21:48 -0500

    Update custom_unet.py

--- keras_unet/models/custom_unet.py
+++ keras_unet/models/custom_unet.py
@@ -22,41 +22,7 @@ from keras.layers import (
         add,
         Activation)
 
-def conv2d_block(
-    inputs,
-    use_batch_norm=True,
-    dropout=0.3,
-    filters=16,
-    kernel_size=(3, 3),
-    activation="relu",
-    kernel_initializer="he_normal",
-    padding="same",
-):
-
-
-    c = Conv2D(
-        filters,
-        kernel_size,
-        activation=activation,
-        kernel_initializer=kernel_initializer,
-        padding=padding,
-        use_bias=not use_batch_norm,
-    )(inputs)
-    if use_batch_norm:
-        c = BatchNormalization()(c)
-    if dropout > 0.0:
-        c = Dropout(dropout)(c)
-    c = Conv2D(
-        filters,
-        kernel_size,
-        activation=activation,
-        kernel_initializer=kernel_initializer,
-        padding=padding,
-        use_bias=not use_batch_norm,
-    )(c)
-    if use_batch_norm:
-        c = BatchNormalization()(c)
-    return c
+from blocks import conv2d_block
 
 
 def custom_unet(
