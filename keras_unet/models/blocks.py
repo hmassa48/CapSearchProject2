27be791632de4ff16c5cf3733ec945e996ec5929
Haley Massa <59619226+hmassa48@users.noreply.github.com>
2020-07-30 22:21:06 -0500

    Create blocks.py

---
+++ keras_unet/models/blocks.py
@@ -0,0 +1,33 @@
+def conv2d_block(
+    inputs,
+    use_batch_norm=True,
+    dropout=0.3,
+    filters=16,
+    kernel_size=(3, 3),
+    activation="relu",
+    kernel_initializer="he_normal",
+    padding="same",
+):
+    c = Conv2D(
+        filters,
+        kernel_size,
+        activation=activation,
+        kernel_initializer=kernel_initializer,
+        padding=padding,
+        use_bias=not use_batch_norm,
+    )(inputs)
+    if use_batch_norm:
+        c = BatchNormalization()(c)
+    if dropout > 0.0:
+        c = Dropout(dropout)(c)
+    c = Conv2D(
+        filters,
+        kernel_size,
+        activation=activation,
+        kernel_initializer=kernel_initializer,
+        padding=padding,
+        use_bias=not use_batch_norm,
+    )(c)
+    if use_batch_norm:
+        c = BatchNormalization()(c)
+    return c
