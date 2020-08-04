873eb1df8178d5f298b730c940a6de7b1bfc9833
Haley Massa <59619226+hmassa48@users.noreply.github.com>
2020-08-03 21:40:32 -0500

    Add files via upload

---
+++ slurm_skinTune.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=HyperSkin
+#SBATCH --partition=wacc
+#SBATCH --cpus-per-task=4
+#SBATCH --time=5-00:00:00
+#SBATCH --output=hyperskin_output-%j.txt
+#SBATCH --gres=gpu:4
+
+
+cd $SLURM_SUBMIT_DIR
+
+
+module load anaconda/wml
+bootstrap_conda
+
+conda activate env697
+
+python skinTune.py 
+
+
