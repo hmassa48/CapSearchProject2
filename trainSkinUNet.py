83d9dee6e94770db8f500a82112bf2057589fe3f
Haley Massa <59619226+hmassa48@users.noreply.github.com>
2020-08-13 17:09:40 -0500

    Update trainSkinUNet.py

--- trainSkinUNet.py
+++ trainSkinUNet.py
@@ -20,25 +20,8 @@ from keras.optimizers import Adam
 from keras_unet.metrics import iou, iou_thresholded
 from keras_unet.losses import bce_dice_loss
 
-
-
 import numpy as np 
 
-def read_in_images(lung_path,msk_path,img_path):
-    Images = []
-    Masks = []
-  
-    for img in img_path:
-        temp_img = lung_path+'/image/' +img
-        temp_img = cv2.imread(temp_img)
-        Images.append(temp_img)
-
-    for msk in msk_path:
-        temp_msk = lung_path+ '/mask/'+msk
-        temp_msk = cv2.imread(temp_msk)
-        Masks.append(temp_msk)
-
-    return Images, Masks
 
 
 #Upload and prepare data 
@@ -55,31 +38,32 @@ def main():
     img_path = skin_path + '/image/'
     msk_path = skin_path + '/mask/'
 
+    #get possible images and masks
     imgs = os.listdir(img_path)
     msks = os.listdir(msk_path)
-    #sort 
+    #sort the images and masks 
     msks = sorted(msks)
     imgs = sorted(imgs)
+     
+    #read in the images 
+    images,masks = read_in_skin_images(skin_path,msks,imgs)
 
-    images,masks = read_in_images(skin_path,msks,imgs)
-
-
-	
+    #preprocess the dataset
     for i in range(0,len(masks)):
         m = masks[i]
-        m = m[:,:,0]
-        m.reshape(m.shape[0],m.shape[1])
-        m = cv2.resize(m,(256,256))
+        m = m[:,:,0] #binarize the mask
+        m.reshape(m.shape[0],m.shape[1]) #reshape the binarized mask
+        m = cv2.resize(m,(256,256)) #resize the skin masks for memory contraints 
         masks[i] = m
         #images
         im = images[i]
-        images[i] = cv2.resize(im,(256,256))
+        images[i] = cv2.resize(im,(256,256)) #resize the images for memory constraints 
 
     #make Arrays 
     images = np.asarray(images)
     masks = np.asarray(masks)
-    masks = masks / 255
-    masks = masks.reshape(masks.shape[0],masks.shape[1],masks.shape[2],1)
+    masks = masks / masks.max() #normalize the masks 
+    masks = masks.reshape(masks.shape[0],masks.shape[1],masks.shape[2],1) #reshape to binarized mask array 
     
 
     #split the data
@@ -109,15 +93,16 @@ def main():
     val_generator = train_datagen.flow(
         img_val, mask_val)
    
-        
-
-
+  
+    #standardize steps per epoch for the dataset and batch size
     STEPS_PER_EPOCH = len(img_train) // 16
+
     #Get U Net 
-
-
+	
     input_shape = img_train[0].shape
+    #use multi GPU strategy to run the custom U-Net
     with strategy.scope():
+	#run the custom model with parameters you want to analyze
         model = custom_unet(
         input_shape,
         filters=64,
@@ -133,7 +118,7 @@ def main():
     
 
     ##Compile and Train
-
+       #save model with this name **Change model name for each run to save model**
         model_filename = 'basic_600_BDice_LR001_BASIC_SKIN_UNET.h5'
         callback_checkpoint = ModelCheckpoint(
         model_filename, 
@@ -149,7 +134,6 @@ def main():
             loss= bce_dice_loss,
             metrics=['mse',iou, iou_thresholded],
     )
-
 
         history = model.fit_generator(
         train_generator,
