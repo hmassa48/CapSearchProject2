e5e8ea28dece7bb07343bfeb1ee67863972e76f1
Haley Massa <59619226+hmassa48@users.noreply.github.com>
2020-08-13 12:06:37 -0500

    Update hyper_unet.py

--- keras_unet/models/hyper_unet.py
+++ keras_unet/models/hyper_unet.py
@@ -8,7 +8,6 @@ import tensorflow.keras as keras
 from tensorflow.keras import layers
 from tensorflow.keras import backend
 
-from blocks import conv2d_block
 from kerastuner.engine import hypermodel
 from ..metrics import iou, iou_thresholded
 
@@ -28,6 +27,43 @@ from tensorflow.keras.layers import (
     Activation,
 )
 
+"""
+Re-used block within all of the U-Net models. Implemented and slightly modified by removing parts from https://github.com/karolzak/keras-unet
+"""
+
+def conv2d_block(
+    inputs,
+    use_batch_norm=True,
+    dropout=0.3,
+    filters=16,
+    kernel_size=(3, 3),
+    activation="relu",
+    kernel_initializer="he_normal",
+    padding="same",
+):
+    c = Conv2D(
+        filters,
+        kernel_size,
+        activation=activation,
+        kernel_initializer=kernel_initializer,
+        padding=padding,
+        use_bias=not use_batch_norm,
+    )(inputs)
+    if use_batch_norm:
+        c = BatchNormalization()(c)
+    if dropout > 0.0:
+        c = Dropout(dropout)(c)
+    c = Conv2D(
+        filters,
+        kernel_size,
+        activation=activation,
+        kernel_initializer=kernel_initializer,
+        padding=padding,
+        use_bias=not use_batch_norm,
+    )(c)
+    if use_batch_norm:
+        c = BatchNormalization()(c)
+    return c
 
 
 class HyperBasicUNet(hypermodel.HyperModel):
