80dd9df514ec11bad845084ac9a0ab35b2d247cf
Haley Massa <59619226+hmassa48@users.noreply.github.com>
2020-08-13 17:15:03 -0500

    Update chestTune.py

--- chestTune.py
+++ chestTune.py
@@ -1,3 +1,7 @@
+"""
+Script to tune the Lung Dataset using the keras-tuner with the HyperBasicUNet class 
+
+"""
 import kerastuner as kt
 import tensorflow as tf
 import numpy as np
@@ -31,57 +35,43 @@ from tensorflow.python.client import device_lib
 print(device_lib.list_local_devices())
 
 
-NUM_EPOCH_SEARCH = 50
-
-def read_in_images(lung_path,msk_path,img_path):
-    Images = []
-    Masks = []
-  
-    for img in img_path:
-        temp_img = lung_path+'/2d_images/' +img
-        temp_img = cv2.imread(temp_img)
-        Images.append(temp_img)
-
-    for msk in msk_path:
-        temp_msk = lung_path+ '/2d_masks/'+msk
-        temp_msk = cv2.imread(temp_msk)
-        Masks.append(temp_msk)
-
-    return Images, Masks
 
 
 
 #load dataset
 lung_path = 'Data/lung-masks'
 
+
 img_path = lung_path + '/2d_images/'
 msk_path = lung_path + '/2d_masks/'
 
+#list possible images and masks
 imgs = os.listdir(img_path)
 msks = os.listdir(msk_path)
 
+#sort images and masks 
 msks = sorted(msks)
 imgs = sorted(imgs)
 
-images,masks = read_in_images(lung_path,msks,imgs)
+#read in images and masks
+images,masks = read_in_lung_images(lung_path,msks,imgs)
 
-
-        
+#basic preprocessing of the dataset
 for i in range(0,len(masks)):
     m = masks[i]
-    m = m[:,:,0]
-    m.reshape(m.shape[0],m.shape[1])
-    m = cv2.resize(m,(256,256))
+    m = m[:,:,0] #binarize the masks
+    m.reshape(m.shape[0],m.shape[1]) #reshape binary mask
+    m = cv2.resize(m,(256,256)) #resize the images based on memory constraints 
     masks[i] = m
     #images
     im = images[i]
-    images[i] = cv2.resize(im,(256,256))
+    images[i] = cv2.resize(im,(256,256)) #resize the images based on memory constraints 
 
 #make Arrays 
 images = np.asarray(images)
 masks = np.asarray(masks)
-masks = masks / 255
-masks = masks.reshape(masks.shape[0],masks.shape[1],masks.shape[2],1)
+masks = masks / masks.max() #normalize the mask images 
+masks = masks.reshape(masks.shape[0],masks.shape[1],masks.shape[2],1) #reshape binarized mask
 
 #split into validations
 img_overall_train, img_test, mask_overall_train, mask_test = train_test_split(images, masks, test_size=0.16667, random_state=42)
@@ -96,9 +86,11 @@ train_generator = train_datagen.flow(
 val_generator = train_datagen.flow(
     img_test, mask_test)
 
-
+#load in hyper U-Net architecture 
 hypermodel = HyperBasicUNet(input_shape = (256,256,3), classes = 1)
 
+#set up hyperband search strategy 
+#parameters set due to memory constraints 
 tuner_hb = Hyperband(
             hypermodel,
             max_epochs=200,
@@ -109,11 +101,13 @@ tuner_hb = Hyperband(
             hyperband_iterations = 3
         )
 
+#print total search space
 tuner_hb.search_space_summary()
 
-
+#search through the total search space
 tuner_hb.search(train_generator,epochs = 500,verbose = 1,validation_data = (img_test,mask_test))
 
+#save best model and hyperparameters
 best_model = tuner_hb.get_best_models(1)[0]
 
 best_hyperparameters = tuner_hb.get_best_hyperparameters(1)[0]
@@ -132,4 +126,4 @@ with open('best_LGG_basicUNet_Param.txt', 'w') as f:
 
 print("Saved model to disk")
 print(best_hyperparameters)
-tuner_hb.results_summary()
+tuner_hb.results_summary() #print best 10 models 
