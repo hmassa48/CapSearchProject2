cb5158083a77b6b86e589c190ac2b6ffa454d349
Haley Massa <59619226+hmassa48@users.noreply.github.com>
2020-08-13 11:58:34 -0500

    Update chestTune.py

--- chestTune.py
+++ chestTune.py
@@ -50,142 +50,6 @@ def read_in_images(lung_path,msk_path,img_path):
 
 
 
-
-
-def conv2d_block(
-    inputs,
-    use_batch_norm=True,
-    dropout=0.3,
-    dropout_type="standard",
-    filters=16,
-    kernel_size=(3, 3),
-    activation="relu",
-    kernel_initializer="he_normal",
-    padding="same",
-):
-
-    DO = Dropout
-    
-
-    c = Conv2D(filters,kernel_size,activation=activation,kernel_initializer=kernel_initializer,
-        padding=padding,use_bias=not use_batch_norm)(inputs)
-    if use_batch_norm:
-        c = BatchNormalization()(c)
-    if dropout > 0.0:
-        c = DO(dropout)(c)
-    c = Conv2D(filters,kernel_size,activation=activation,kernel_initializer=kernel_initializer,
-        padding=padding,use_bias=not use_batch_norm)(c)
-    if use_batch_norm:
-        c = BatchNormalization()(c)
-    return c
-
-
-def build_model(hp):
-    
-    input_shape = (256,256,3)
-    classes = 1
-    
-   # filters = 32
-    filters = hp.Choice('filters',[8,16,32,64],default = 32)
-    activation = hp.Choice('activation',['relu','elu'],default = 'relu')
-    # activation= 'relu'
-   # use_batch_norm = True
-    use_batch_norm = hp.Boolean('use_batch_norm',default = False)
-    dropout = hp.Float('dropout', 0, 0.6, 0.05, default = 0)
-    dropout_change_per_layer = hp.Float('dropout_change_per_layer',0,0.05,0.005)
-     #dropout_change_per_layer = 0.0
-   # use_dropout_on_upsampling = False
-    use_dropout_on_upsampling = hp.Boolean('use_dropout_on_upsampling',default = False)
-   # decoder_type = 'simple'
-    decoder_type = hp.Choice('decoder_type',['simple','transpose'],default = 'simple')
-   # dropout = 0.3
- 
-    dropout_type = 'standard'
-    strides = (2,2)
-    output_activation='sigmoid'
-   # use_dropout_on_upsampling = False 
-        
-    # Build U-Net model
-    inputs = Input(input_shape)
-    x = inputs
-
-    down_layers = []
-    for l in range(hp.Int('num_layers',3,5,1)):
-        x = conv2d_block(
-            inputs=x,
-            filters=filters,
-            use_batch_norm=use_batch_norm,
-            dropout=dropout,
-            dropout_type=dropout_type,
-            activation=activation,
-        )
-        down_layers.append(x)
-        x = MaxPooling2D((2, 2))(x)
-        dropout += dropout_change_per_layer
-        filters = filters * 2  # double the number of filters with each layer
-
-    x = conv2d_block(
-        inputs=x,
-            filters=filters,
-            use_batch_norm=use_batch_norm,
-            dropout=dropout,
-            dropout_type=dropout_type,
-            activation=activation,
-        )
-
-    if not use_dropout_on_upsampling:
-        dropout = 0.0
-        dropout_change_per_layer = 0.0
-
-    for conv in reversed(down_layers):
-        filters //= 2  # decreasing number of filters with each layer
-        dropout -= dropout_change_per_layer
-        if decoder_type == 'transpose':
-            x = Conv2DTranspose(filters, (3,3), strides=(2,2),padding='same', use_bias=not(use_batch_norm))(x)
-            if use_batch_norm:
-                x = BatchNormalization()(x)
-            x = Activation(activation)(x)
-            x = concatenate([x, conv])
-            x = Conv2D(filters, (3,3), padding="same",  use_bias=not(use_batch_norm))(x)
-            if use_batch_norm:
-                x = BatchNormalization()(x)
-            x = Activation(activation)(x)
-
-        else:
-            x = UpSampling2D((2,2))(x)
-            x = concatenate([x, conv])
-            x = conv2d_block(
-                inputs=x,
-                filters=filters,
-                use_batch_norm=use_batch_norm,
-                dropout=dropout,
-                dropout_type=dropout_type,
-                activation=activation,
-            )
-
-    x = Conv2D(classes, (1, 1), activation=output_activation)(x)
-
-    model = Model(inputs, x, name='UNet')
-    optimizer_name ='adam'
-    optimizer = keras.optimizers.get(optimizer_name)
-    optimizer.learning_rate = 0.001
-    model.compile(optimizer=optimizer,loss=bce_dice_loss,metrics=[iou,iou_thresholded])
-    
-    return model
-
-tuner_hb = Hyperband(
-            build_model,
-            max_epochs=200,
-            objective='val_loss',
-            metrics = [iou,iou_thresholded,'mse'],
-            distribution_strategy=tf.distribute.MirroredStrategy(),
-            seed=42,
-            hyperband_iterations = 3
-        )
-
-tuner_hb.search_space_summary()
-
-
 #load dataset
 lung_path = 'lung-masks'
 
@@ -230,6 +94,22 @@ train_generator = train_datagen.flow(
 
 val_generator = train_datagen.flow(
     img_test, mask_test)
+
+
+hypermodel = HyperUNet(
+
+tuner_hb = Hyperband(
+            hypermodel,
+            max_epochs=200,
+            objective='val_loss',
+            metrics = [iou,iou_thresholded,'mse'],
+            distribution_strategy=tf.distribute.MirroredStrategy(),
+            seed=42,
+            hyperband_iterations = 3
+        )
+
+tuner_hb.search_space_summary()
+
 
 
 tuner_hb.search(train_generator,epochs = 500,verbose = 1,validation_data = (img_test,mask_test))
